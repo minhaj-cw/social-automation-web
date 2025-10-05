f888b3c0f645371dbd7bd71a416b2281a854737c
Minhaj <minhaj.codeware@gmail.com>
2025-10-05 16:55:39 +0600

    added requirements.txt

--- core/urls.py
+++ core/urls.py
@@ -14,13 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# core/urls.py
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('dashboard/', include('accounts.urls')),  # now 'path' is defined
-    path('', RedirectView.as_view(url='/accounts/login/')),
+    path('dashboard/', include('accounts.urls')),
 ]
+
+# Serve static files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
